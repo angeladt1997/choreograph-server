6472f9ac145c0169f7da1274df29baf6490b0694
unknown <kimgeladavis@gmail.com>
2020-10-05 19:59:10 -0500

    hope this fixes chai

--- test/setup.js
+++ test/setup.js
@@ -3,7 +3,7 @@ process.env.TZ = 'UTC'
 process.env.NODE_ENV = 'test'
 
 require('dotenv').config()
-const { expect } = require('chai')
+const expect = require("chai").expect;
 const supertest = require('supertest')
 
 global.expect = expect
